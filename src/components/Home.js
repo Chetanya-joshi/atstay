54305cdc2b3a6e38b984ef384e852266e466dcfb
Chetanya-joshi <108217245+Chetanya-joshi@users.noreply.github.com>
2023-09-01 10:55:51 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Navbar'
+import HomeComponents from './HomeComponents';
+export default function Home() {
+  return (
+    <div>
+        <Navbar />
+        <HomeComponents />
+    </div>
+  )
+}
